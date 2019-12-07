1c0707af50075f2bff7fb0ff00fe30f879ef9b85
negier <1208883805@qq.com>
2019-12-07 18:19:34 +0800

    pcm 上一个版本内存占用高，移动设备会崩溃，所以代码改成了直接写进文件，不是先写入数组

--- Code/pcm_mono_to_stereo.c
+++ Code/pcm_mono_to_stereo.c
@@ -9,8 +9,12 @@ int main(){
  * 单声道的PCM转双声道
  */
 void pcmMonoToStereo(const char* pcmPath,const char* newPcmPath){
+	if(sizeof(char)!=1){
+		printf("这只适用于char为8位的机器");
+		return;
+	}
 	FILE* pcm;
-	if((pcm =  fopen(pcmPath,"r+"))==NULL){
+	if((pcm =  fopen(pcmPath,"r"))==NULL){
 		printf("传入的pcm文件地址有误");
 		return;
 	}
@@ -21,24 +25,25 @@ void pcmMonoToStereo(const char* pcmPath,const char* newPcmPath){
         rewind(pcm);
         
 	FILE* newPcm =  fopen(newPcmPath,"w");
-        char newPcmArray[fileSize*2]; //单声道转双声道，自然大小要翻倍
 
-        int p=0;
-        int i=0,m = 0;
+        int p;
+        int mark = 0;
+	char left1,left2;
         while((p=fgetc(pcm))!=EOF){ // fgetc返回的是一个unsigned char，一个字节
-                if(m==2){
+		if(mark==0){
+			left1 = p;
+		}else if(mark==1){
+			left2 = p;
+		}else if(mark==2){
                         //复制值到另一个声道
-                        newPcmArray[i] = newPcmArray[i-2];
-                        newPcmArray[i+1] = newPcmArray[i-1];
-                       
-		       	i += 2;
-                        m = 0;
+			fwrite(&left1,sizeof(left1),1,newPcm);
+ 			fwrite(&left2,sizeof(left2),1,newPcm);
+
+                        mark = 0;
                 }
-                newPcmArray[i] = p;
-                m++;
-                i++;
+		fwrite(&p,1,1,newPcm);
+                mark++;
         }
-	fwrite(&newPcmArray,sizeof(newPcmArray),1,newPcm);
 	
 	fclose(pcm);
 	fclose(newPcm);
